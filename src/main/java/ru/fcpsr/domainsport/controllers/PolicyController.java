1f69fb1ddba642938f747f5a844310f08f1b0ab9
Ivan Morgan <morgantdarck@gmail.com>
2024-04-11 17:48:46 +0300

    update 11.04.2024 * Начал добавление политики безопасности

---
+++ src/main/java/ru/fcpsr/domainsport/controllers/PolicyController.java
@@ -0,0 +1,23 @@
+package ru.fcpsr.domainsport.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/policy")
+public class PolicyController {
+    @GetMapping("/secure")
+    public Mono<Rendering> securityPolicyPage(){
+        return Mono.just(
+                Rendering.view("template")
+                        .modelAttribute("title","Политика безопасности")
+                        .modelAttribute("index","policy-secure-page")
+                        .build()
+        );
+    }
+}
